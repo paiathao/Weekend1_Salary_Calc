14e92dcb46d6f56805fd57ed420aebc981f9fc0d
Paia Thao <paiathao09@gmail.com>
2018-06-24 22:48:15 -0500

    “update”

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let employees = [     // object worker = {name,last,id,title,annualSay}
-    {
-        name : 'Jen',
-        last : 'Barber',
-        id : 4521,
-        title : 'Team Lead',
-        annualSal : 80000,
-    }, {
-        name : 'Maurice',
-        last : 'Moss',
-        id : 8721,
-        title : 'Support Team',
-        annualSal : 58000,
-    }, {
-        name : 'Roy',
-        last : 'Smith',
-        id : 9623,
-        title : 'Quality Assurance',
-        annualSal : 48000,
-    }
+    // {
+    //     name: 'Jen',
+    //     last: 'Barber',
+    //     id: 4521,
+    //     title: 'Team Lead',
+    //     annualSal: 80000,
+    // }, {
+    //     name: 'Maurice',
+    //     last: 'Moss',
+    //     id: 8721,
+    //     title: 'Support Team',
+    //     annualSal: 58000,
+    // }, {
+    //     name: 'Roy',
+    //     last: 'Smith',
+    //     id: 9623,
+    //     title: 'Quality Assurance',
+    //     annualSal: 48000,
+    // }
 ];
 
 
@@ -25,67 +25,87 @@ let employees = [     // object worker = {name,last,id,title,annualSay}
 $(document).ready(readyNow);
 
 function readyNow() {
-    addClickHandlers ();
-    setTotalMonthly ();
+
+    let totalMonthly = 0;
+    addClickHandlers();
+    setTotalMonthly();
     for (let employ of employees) {
-    let $row = $('<tr></tr>');
-     $row.append(`<td>${employ.name}</td>`)   
-     $row.append(`<td>${employ.last}</td>`) 
-     $row.append(`<td>${employ.id}</td>`) 
-     $row.append(`<td>${employ.title}</td>`) 
-     $row.append(`<td>${employ.annualSal}</td>`) 
-
-     $('#tableBody').append($row);
+        let $row = $('<tr></tr>');
+        $row.append(`<td>${employ.name}</td>`)
+        $row.append(`<td>${employ.last}</td>`)
+        $row.append(`<td>${employ.id}</td>`)
+        $row.append(`<td>${employ.title}</td>`)
+        $row.append(`<td>${employ.annualSal}</td>`)
+        $row.append(`<td><button class='deleteBtn'>DELETE</button></td>`)
+        $('#tableBody').append($row);
 
     }
 
-    function addClickHandlers () {
-        $ ('#submit').on('click', handleSubmit);
+    function addClickHandlers() {
+        $('#submit').on('click', handleSubmit);
+        $('#tableBody').on('click', '.deleteBtn', handleDelete);
     }
 
-    function handleSubmit () {
-       let firstName = $('#firstName').val();
-       let lastName = $('#lastName').val();
-       let employId = $('#employId').val();
-       let title = $('#title').val();
-       let annualSal = parseInt($('#annualSalary').val());   //when storing data, convert string to number
+    function handleSubmit() {
+        let firstName = $('#firstName').val();
+        let lastName = $('#lastName').val();
+        let employId = $('#employId').val();
+        let title = $('#title').val();
+        let annualSal = parseInt($('#annualSalary').val());   //when storing data, convert string to number
 
         //creating new object with the attribute inputs above. Then add new object to array
-       let newEmployee =  {
-        name : firstName,    
-        last : lastName,
-        id : employId,
-        title : title,
-        annualSal : annualSal,   
-    }
+        let newEmployee = {
+            name: firstName,
+            last: lastName,
+            id: employId,
+            title: title,
+            annualSal: annualSal,
+        }
         //pushing the new object to employees array
-        employees.push(newEmployee);  
+        employees.push(newEmployee);
+        
+        let $row;
+        //append to DOM
+        for (let i = 0; i < employees.length; i++) {   //assigning a index to each row created
 
-       //append to DOM
-       let $row = $('<tr></tr>');
-       $row.append(`<td>${firstName}</td>`)   
-       $row.append(`<td>${lastName}</td>`) 
-       $row.append(`<td>${employId}</td>`) 
-       $row.append(`<td>${title}</td>`) 
-       $row.append(`<td>${annualSal}</td>`) 
+            $row = $('<tr data-index="' + i + '"></tr>');
+            $row.append(`<td>${firstName}</td>`)
+            $row.append(`<td>${lastName}</td>`)
+            $row.append(`<td>${employId}</td>`)
+            $row.append(`<td>${title}</td>`)
+            $row.append(`<td>${annualSal}</td>`)
+            $row.append(`<td><button class='deleteBtn'>DELETE</button></td>`)
 
-       $('#tableBody').append($row);
+        }
 
-       //clear input val
-       $('#firstName').val('');
-       $('#lastName').val('');
-       $('#employId').val('');
-       $('#title').val('');
-       $('#annualSalary').val('');
+        $('#tableBody').append($row);
 
-       console.log(employees);
+        //clear input val
+        $('#firstName').val('');
+        $('#lastName').val('');
+        $('#employId').val('');
+        $('#title').val('');
+        $('#annualSalary').val('');
+
+        console.log(employees);
         //call function for total salary
-       setTotalMonthly ();
+        setTotalMonthly();
+    }
+
+    function handleDelete() {
+        let row = $(this).closest('tr')
+        let index = $(this).closest('tr').data('index')  //data targeting index
+        //remove that index of that employee from the array
+        employees.splice(index, 1)
+        row.remove();
+        // update the total again by calling the total function
+        setTotalMonthly();
+        console.log(employees);
+
     }
 
     //access elements in array, if elements is an object, access key
-    function setTotalMonthly () {
-        let totalMonthly = 0;
+    function setTotalMonthly() {
         for (let i = 0; i < employees.length; i++) {   // looping through the array
             //employee[i] == worker
             //employee[i].anualSay == worker.anualSal
@@ -93,15 +113,15 @@ function readyNow() {
         }
 
         if (totalMonthly > 20000) {
-            $('#totalMonthly').css( "background-color", "red" );
+            $('#totalMonthly').css("background-color", "red");
         } else {
-            $('#totalMonthly').css( "background-color", "white" );
+            $('#totalMonthly').css("background-color", "white");
         }
 
         console.log(totalMonthly);
 
-        $('#totalMonthly').text('Total Monthly: ' + (Math.round(totalMonthly)));  
+        $('#totalMonthly').text((Math.round(totalMonthly)));
     }
-    
+
 }
 
